aea0e1e2adf6470533c044a878f7b0cfe3b071ce
Vlad Nicula <vladnicula@gmail.com>
2024-11-12 09:54:56 +0200

    feat: add a prompt template generation ndoe

---
+++ nodes/TemplateStringNode/TemplateStringNode.node.ts
@@ -0,0 +1,32 @@
+import type {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+
+export class TemplateStringNode implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Template String Node',
+		name: 'templateStringNode',
+		group: ['transform'],
+		version: 1.1,
+		description: 'Allows you to manipulate a multi line text message for usage in later nodes.',
+		defaults: {
+			name: 'Template String Node',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		properties: [],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+
+		const itemsWithDescription = items.map((item) => {
+			return { json: { description: item.json.description } }
+		})
+
+		return [itemsWithDescription]
+	}
+}
